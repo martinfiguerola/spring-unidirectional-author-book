e93697378568729eaf36fb22c88d7e77c921980f
martinfiguerola <fedefiguerola@gmail.com>
2025-06-26 08:35:29 +0200

    feat(book): add GET all, GET by ID, PUT and DELETE endpoints
    - Implemented controller methods for basic CRUD operations
    - Added service interface and implementation logic

--- src/main/java/com/martin/authorbook/controller/BookController.java
+++ src/main/java/com/martin/authorbook/controller/BookController.java
@@ -2,13 +2,14 @@ package com.martin.authorbook.controller;
 
 import com.martin.authorbook.dto.book.BookRequestDTO;
 import com.martin.authorbook.dto.book.BookResponseDTO;
+import com.martin.authorbook.entity.Book;
 import com.martin.authorbook.service.book.BookService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
@@ -25,5 +26,34 @@ public class BookController {
     public ResponseEntity<BookResponseDTO> createBook (@RequestBody BookRequestDTO book) {
         BookResponseDTO bookResponseDTO = service.save(book);
         return ResponseEntity.status(HttpStatus.CREATED).body(bookResponseDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookResponseDTO> getBook (@PathVariable Long id) {
+        return service.findById(id)
+                .map(book -> ResponseEntity.status(HttpStatus.OK).body(book))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookResponseDTO>> getBooks () {
+        List<BookResponseDTO> bookResponseDTOS = service.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(bookResponseDTOS);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BookResponseDTO> updateBook (@PathVariable Long id, @RequestBody BookRequestDTO bookRequestDTO) {
+        Optional<BookResponseDTO> optionalBookResponseDTO = service.update(id, bookRequestDTO);
+        return optionalBookResponseDTO
+                .map( responseDTO ->  ResponseEntity.status(HttpStatus.OK).body(responseDTO))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteBook (@PathVariable Long id) {
+        if(service.deleteById(id)) return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Book with the given ID does not exist.");
     }
 }
--- src/main/java/com/martin/authorbook/service/book/BookServiceImpl.java
+++ src/main/java/com/martin/authorbook/service/book/BookServiceImpl.java
@@ -7,6 +7,9 @@ import com.martin.authorbook.mapper.BookMapper;
 import com.martin.authorbook.repository.BookRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class BookServiceImpl implements BookService {
 
@@ -27,5 +30,48 @@ public class BookServiceImpl implements BookService {
 
         // Step 3: Convert the saved entity into a DTO and return it
         return BookMapper.toDTO(savedBook);
+    }
+
+    @Override
+    public Optional<BookResponseDTO> findById(Long id) {
+
+        Optional<Book> optionalBook = repository.findById(id);
+
+        return optionalBook.map(BookMapper::toDTO);
+    }
+
+    @Override
+    public List<BookResponseDTO> findAll() {
+
+        List<Book> books = repository.findAll();
+
+        return books.stream()
+                .map(BookMapper::toDTO)
+                .toList();
+    }
+
+    @Override
+    public Optional<BookResponseDTO> update(Long id, BookRequestDTO bookRequestDTO) {
+        Optional<Book> optionalBook = repository.findById(id);
+
+        return optionalBook.map(book -> {
+            book.setTitle(bookRequestDTO.getTitle());
+            book.setIsbn(bookRequestDTO.getIsbn());
+
+            Book savedBook = repository.save(book);
+
+            return BookMapper.toDTO(savedBook);
+        });
+    }
+
+    @Override
+    public Boolean deleteById(Long id) {
+        Optional<Book> optionalBook = repository.findById(id);
+
+        return optionalBook.map(book -> {
+            repository.delete(book);
+            return true;
+        }).orElse(false);
+
     }
 }
